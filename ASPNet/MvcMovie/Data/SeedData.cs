19379ca6976505d4b6104a23559cc651c9f9386f
Jintao <xiaojintao@outlook.com>
2023-06-10 12:25:59 +0800

    add mvc

---
+++ ASPNet/MvcMovie/Data/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var ctx = new MvcMovieContext(
+                   serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
+        {
+            if (ctx.Movie.Any())
+            {
+                return;
+            }
+            
+            ctx.AddRange(
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M,
+                    Rating = "R"
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M,
+                    Rating = "R12"
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M,
+                    Rating = "R20"
+                },
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M,
+                    Rating = "R2"
+                });
+            ctx.SaveChanges();
+        }
+    }
+}
